7ed6e1d3e38cf48661123ee8ceab8d5271277c49
palomaborro <palomabs93@gmail.com>
2023-11-14 12:04:45 +0100

    Add MongoDB certificate

--- index.js
+++ index.js
@@ -7,20 +7,36 @@ require("dotenv").config();
 const dbConnectionURL =
   process.env.DATABASE_URL || "mongodb://localhost:27017/spotify_project";
 
-console.log(dbConnectionURL);
-
-mongoose
-  .connect(dbConnectionURL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("Connected to Database");
-
-    app.listen(port, () => {
-      console.log(`Server running on port ${port}`);
+const writeMongoDBCertificate = (cert) => {
+  return new Promise((resolve, reject) => {
+    const fs = require("fs");
+    fs.writeFile("certificate.pem", cert, (err) => {
+      if (err) {
+        reject(err);
+      }
+      resolve("Certificate saved");
     });
-  })
-  .catch((err) => {
-    console.error("Database connection error:", err);
   });
+};
+
+writeMongoDBCertificate(process.env.MONGODB_CERTIFICATE).then(() =>
+  mongoose
+    .connect(dbConnectionURL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      sslValidate: true,
+      tlsCertificateKeyFile: "certificate.pem",
+      authMechanism: "MONGODB-X509",
+      authSource: "$external",
+    })
+    .then(() => {
+      console.log("Connected to Database");
+
+      app.listen(port, () => {
+        console.log(`Server running on port ${port}`);
+      });
+    })
+    .catch((err) => {
+      console.error("Database connection error:", err);
+    })
+);
