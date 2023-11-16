2bbb345e1600e7555d0d32a37662cf0a30083bc0
palomaborro <palomabs93@gmail.com>
2023-11-16 12:41:49 +0100

    Use temp folder for the mongodb certificate

--- index.js
+++ index.js
@@ -7,22 +7,41 @@ require("dotenv").config();
 const dbConnectionURL =
   process.env.DATABASE_URL || "mongodb://localhost:27017/spotify_project";
 
-mongoose
-  .connect(dbConnectionURL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    sslValidate: true,
-    sslCA: Buffer.from(process.env.MONGODB_CERTIFICATE, "base64"),
-    authMechanism: "MONGODB-X509",
-    authSource: "$external",
-  })
-  .then(() => {
-    console.log("Connected to Database");
+const writeMongoDBCertificate = (cert) => {
+  return new Promise((resolve, reject) => {
+    const fs = require("fs");
+    const path = require("path");
+    const tempPath = path.join("/tmp", "certificate.pem");
 
-    app.listen(port, () => {
-      console.log(`Server running on port ${port}`);
+    fs.writeFile(tempPath, cert, (err) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(tempPath);
     });
-  })
-  .catch((err) => {
-    console.error("Database connection error:", err);
   });
+};
+
+writeMongoDBCertificate(
+  Buffer.from(process.env.MONGODB_CERTIFICATE, "base64")
+).then((tempPath) =>
+  mongoose
+    .connect(dbConnectionURL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      sslValidate: true,
+      tlsCertificateKeyFile: tempPath,
+      authMechanism: "MONGODB-X509",
+      authSource: "$external",
+    })
+    .then(() => {
+      console.log("Connected to Database");
+
+      app.listen(port, () => {
+        console.log(`Server running on port ${port}`);
+      });
+    })
+    .catch((err) => {
+      console.error("Database connection error:", err);
+    })
+);
