f83533528153780462d40a7f8173eeb9075c8f59
palomaborro <palomabs93@gmail.com>
2023-11-16 11:52:30 +0100

    Remove mongoDB write certificate function

--- index.js
+++ index.js
@@ -7,38 +7,22 @@ require("dotenv").config();
 const dbConnectionURL =
   process.env.DATABASE_URL || "mongodb://localhost:27017/spotify_project";
 
-const writeMongoDBCertificate = (cert) => {
-  return new Promise((resolve, reject) => {
-    const fs = require("fs");
-    fs.writeFile("certificate.pem", cert, (err) => {
-      if (err) {
-        reject(err);
-      }
-      resolve("Certificate saved");
+mongoose
+  .connect(dbConnectionURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    sslValidate: true,
+    sslCA: Buffer.from(process.env.MONGODB_CERTIFICATE, "base64"),
+    authMechanism: "MONGODB-X509",
+    authSource: "$external",
+  })
+  .then(() => {
+    console.log("Connected to Database");
+
+    app.listen(port, () => {
+      console.log(`Server running on port ${port}`);
     });
+  })
+  .catch((err) => {
+    console.error("Database connection error:", err);
   });
-};
-
-writeMongoDBCertificate(
-  Buffer.from(process.env.MONGODB_CERTIFICATE, "base64")
-).then(() =>
-  mongoose
-    .connect(dbConnectionURL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      sslValidate: true,
-      tlsCertificateKeyFile: "certificate.pem",
-      authMechanism: "MONGODB-X509",
-      authSource: "$external",
-    })
-    .then(() => {
-      console.log("Connected to Database");
-
-      app.listen(port, () => {
-        console.log(`Server running on port ${port}`);
-      });
-    })
-    .catch((err) => {
-      console.error("Database connection error:", err);
-    })
-);
